2a51d0e5ef527438ae0c70166b806b2860414fc7
liucunzhou <liucunzhou@163.com>
2020-05-13 10:08:18 +0800

    's'

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ Vue.use(qs);
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
-Vue.prototype.host = 'http://crm.reactphp.club';
+Vue.prototype.host = 'http://a.reactphp.club';
 
 new Vue({
     router,
